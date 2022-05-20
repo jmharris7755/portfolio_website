679d8a449d0117af7eb829cf8b3a358a7e0b279c
Justin Harris <43102944+jmharris7755@users.noreply.github.com>
2022-05-20 00:49:55 -0700

    Updated Menu, intro and added assets
    Added options to the menu to go to different sections of the webpage
    
    Added circle to intro section to hold picture. Added greeting and about/title information to intro with typewriter effect.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,34 @@
 import "./intro.scss"
+import Typewriter from 'typewriter-effect'
 
 export default function Intro() {
+
     return(
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello! I'm</h2>
+                    <h1>Justin Harris</h1>
+                    <h3>CS Graduate 
+                        <span>
+                            <Typewriter
+                                options={{
+                                    strings: ["University of Idaho", "Software Engineer"],
+                                    autoStart: true,
+                                    delay:75,
+                                    loop:true
+                            }}/>
+                        </span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/downArrow.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
